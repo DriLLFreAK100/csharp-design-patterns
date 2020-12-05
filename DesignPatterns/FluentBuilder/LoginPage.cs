49d6fb17e40f3b723f20e0778fcc40f61b926ce6
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-12-05 09:28:05 +0800

    feat: fluent builder

---
+++ DesignPatterns/FluentBuilder/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FluentBuilder
+{
+    public class LoginPage
+    {
+        public LoginPage EnterUsername(string username)
+        {
+            // Get username input element, fill it up, etc.
+            Console.WriteLine($"Entered username: {username}");
+            return this;
+        }
+
+        public LoginPage EnterPassword(string password)
+        {
+            // Get password input element, fill it up, etc.
+            Console.WriteLine($"Entered password: {password}");
+            return this;
+        }
+
+        public void ClickLoginButton()
+        {
+            // Get login button element, invoke a click
+            Console.WriteLine("Clicked Login Button");
+        }
+    }
+}
---
+++ DesignPatterns/FluentBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Fluent Builder focuses on providing Method Chaining capability to an Object
+/// In this example, we will demonstrate the pattern by chaining UI commands, i.e. fill username and password to login.
+/// </summary>
+namespace FluentBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginPage pageObject = new LoginPage();
+
+            // A usage/application of Fluent Builder
+            pageObject
+                .EnterUsername("Tester")
+                .EnterPassword("Tester Password")
+                .ClickLoginButton();
+        }
+    }
+}
